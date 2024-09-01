7c3bd0af7508eedec170daa74abaeb8bcef79d91
unknown <khalidabdi3101@gmail.com>
2024-09-01 14:46:54 +0300

    done tip cal

--- challenge.js
+++ challenge.js
@@ -1,2 +1,15 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let bill = 49
+
+if(bill >= 50 && bill <= 300){
+    tip = bill * 0.15;
+    
+}
+
+if (bill<=50) {
+    tip= bill * 0.20
+}
+console.log(tip)
+
+
